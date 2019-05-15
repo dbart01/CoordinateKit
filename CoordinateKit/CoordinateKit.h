e8bb46e0e4f1dcc42dd241dfdb8bbee8dd0cf843
Dima Bart <dima.bart01@gmail.com>
2019-05-15 15:41:56 -0400

    Initial Commit

---
+++ CoordinateKit/CoordinateKit.h
@@ -0,0 +1,19 @@
+//
+//  CoordinateKit.h
+//  CoordinateKit
+//
+//  Created by Dima Bart on 2019-05-15.
+//  Copyright © 2019 Dima Bart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoordinateKit.
+FOUNDATION_EXPORT double CoordinateKitVersionNumber;
+
+//! Project version string for CoordinateKit.
+FOUNDATION_EXPORT const unsigned char CoordinateKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoordinateKit/PublicHeader.h>
+
+
